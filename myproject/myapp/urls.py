503a724f71af0f314cc056948778b173d490da23
alpro <alprokh64@gmail.com>
2025-03-07 15:27:23 +0200

    v1 server

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AuthorViewSet, BookViewSet
+from django.urls import path
+from . import views
+
 
 router = DefaultRouter()
 router.register(r'authors', AuthorViewSet)
@@ -8,4 +11,11 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+]
+
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
 ]
